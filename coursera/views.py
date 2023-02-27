0a27b2851aed0bde3e001770072bb7b7cea409bc
Vivek <vivekyadav138001@gmail.com>
2023-02-27 17:28:53 +0530

    commit

--- coursera/views.py
+++ coursera/views.py
@@ -2,8 +2,5 @@
 
 from django.shortcuts import HttpResponse
 
-def home(request):
-        return HttpResponse("Hello, world. 9729df58 is the polls index.")
-
 def owner(request):
-        return HttpResponse("Hello, world. ba077326 is the polls index.")
+        return HttpResponse("Hello, world. ba077326 is the polls owner.")
